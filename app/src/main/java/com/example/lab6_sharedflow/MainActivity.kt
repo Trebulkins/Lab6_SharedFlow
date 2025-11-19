4615158e2021d590369f9135ea634237f6afc7c0
Trebulkins <trebinivan2005@gmail.com>
2025-11-19 10:36:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab6_sharedflow/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.lab6_sharedflow
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lab6_sharedflow.ui.theme.Lab6_SharedFlowTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab6_SharedFlowTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ScreenSetup(innerPadding as Modifier)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ScreenSetup(modifier: Modifier = Modifier) {
+    MainScreen(modifier)
+}
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lab6_SharedFlowTheme {
+        ScreenSetup()
+    }
+}
